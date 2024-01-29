51070470368dfda50d2fa454c9814ed861f5a44d
NicolasAponte <nico.aponte98@hotmail.com>
2024-01-29 08:00:00 -0500

    Get tweet logic (#10)
    Co-authored-by: Nicolas Aponte <nicolas.aponte@mercadolibre.com>

--- models/tweet.go
+++ models/tweet.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Tweet struct {
 	Message string `bson:"message" json:"message"`
@@ -10,4 +14,11 @@ type SavedTweet struct {
 	UserID  string    `bson:"user_id" json:"user_id,omitempty"`
 	Message string    `bson:"message" json:"message,omitempty"`
 	Date    time.Time `bson:"date" json:"date,omitempty"`
+}
+
+type SelectedTweet struct {
+	ID      primitive.ObjectID `bson:"_id" json:"_id,omitempty"`
+	UserID  string             `bson:"user_id" json:"user_id,omitempty"`
+	Message string             `bson:"message" json:"message,omitempty"`
+	Date    time.Time          `bson:"date" json:"date,omitempty"`
 }
